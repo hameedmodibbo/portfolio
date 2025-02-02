8df4d67a844da96221860df14923a83960403eb0
hameedmodibbo <hameedkomsiri6656@gmail.com>
2025-02-02 12:53:59 +0100

    today

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const toggle = document.querySelector(".toggle");
 const mobileNav = document.querySelector(".mobile-nav");
+const about = document.querySelector('#about');
+const hero = document.querySelector('.hero');
 
 
 toggle.addEventListener('click', ()=>{
   mobileNav.classList.toggle("open")
 })
+const message = document.createElement('div');
+// message.classList('cookie-message')
+message.innerHTML = 'We use this cookie to provide better functionality and analytics<button class="cta btn-close-cookie">Got it!</button>';
+console.log(hero);
+message.classList.add('cookie-message')
+about.append(message)
+console.log(message);
+
+const closeCookie = document.querySelector('.btn-close-cookie');
+closeCookie.addEventListener('click',()=>{
+  about.removeChild(message)
+});
