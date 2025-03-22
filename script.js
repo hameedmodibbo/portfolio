9b34f0beb729c6a168618c50bfe9339a40f034cd
hameedmodibbo <hameedkomsiri6656@gmail.com>
2025-03-23 00:55:56 +0100

    lol

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ const navLinks = document.querySelector('.mobile-nav');
 toggle.addEventListener('click', ()=>{
   mobileNav.classList.toggle("open");
 })
-const message = document.createElement('div');
-message.innerHTML = 'We use this cookie to provide better functionality and analytics<button class="cta btn-close-cookie">Got it!</button>';
-console.log(hero);
-message.classList.add('cookie-message')
-about.append(message)
+// const message = document.createElement('div');
+// message.innerHTML = 'We use this cookie to provide better functionality and analytics<button class="cta btn-close-cookie">Got it!</button>';
+// console.log(hero);
+// message.classList.add('cookie-message')
+// about.append(message)
 
-const closeCookie = document.querySelector('.btn-close-cookie');
-closeCookie.addEventListener('click',()=>{
-  about.removeChild(message)
-});
+// const closeCookie = document.querySelector('.btn-close-cookie');
+// closeCookie.addEventListener('click',()=>{
+//   about.removeChild(message)
+// });
 
 // navLinks.addEventListener('click', function(e){
 // if (e.target.classList.contains('nav-link')) {
